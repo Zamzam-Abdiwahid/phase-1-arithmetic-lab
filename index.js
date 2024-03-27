1d18b2eb810400b91187e92121f9e0f34f6c2d02
Zamzam Abdiwahid <zamzamabdiwahid123@gmail.com>
2024-03-27 18:25:58 +0300

    uses arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1;
+let  num2;
+let multiply;
+num1 = 31, num2 =2;
+multiply = num1  * num2;
+console.log(multiply);
+let random= Math.floor(Math.random()>0) + 1;
+Number.isInteger(random);
+console.log (random);
+
+let mod;
+let num3;
+let num4;
+num3 =5, num4 =19;
+mod = num4 % num3;
+console.log (mod);
+
+let max= Math.max(10,5,6,8,20);
+console.log(max);
